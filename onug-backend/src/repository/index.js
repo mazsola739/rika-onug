2fecaa2a143599bccf1a5f494777849c6ac7b61c
Sylwolveryn <sylwolveryn@gmail.com>
2024-02-20 19:52:16 +0100

    keep repository as require but use ES6 export const - it might just work

--- onug-backend/src/repository/index.js
+++ onug-backend/src/repository/index.js
@@ -1,10 +1,7 @@
-const repositoryType = process.env.ONUG_REPOSITORY_TYPE || 'memory'
-const repositoryPath = `../repository/${repositoryType}-repository`
-const repository = require(repositoryPath)
 import { logDebug } from "../log";
 
-logDebug(`Chosen repository type: ${repositoryType}`)
+const repositoryType = process.env.ONUG_REPOSITORY_TYPE || 'memory'
+const repositoryPath = `../repository/${repositoryType}-repository`
+export const repository = require(repositoryPath)
 
-export default {
-  repository,
-};
+logDebug(`Chosen repository type: ${repositoryType}`)
