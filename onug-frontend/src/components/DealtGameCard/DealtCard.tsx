2abe7c24718f571358766589ac9c5dfeb2258f50
Rika <mazsola39@gmail.com>
2024-03-21 10:47:36 +0100

    deluxe edition

--- onug-frontend/src/components/DealtGameCard/DealtCard.tsx
+++ onug-frontend/src/components/DealtGameCard/DealtCard.tsx
@@ -10,10 +10,26 @@ export const DealtCard: React.FC<DealtCardProps> = observer(
     const { hasMarks } = gameTableStore
     const card = id === 0 ? '' : deckStore.getCardById(id)
     const playerTokenName = position.replace(/^player_/, '')
-    const imgSrc =
-      card && card.id !== 0
-        ? `/assets/playingcards/${card.card_name}.png`
-        : '/assets/playingcards/card_background.png'
+
+    let imgSrc = card && card.id !== 0
+      ? `/assets/playingcards/${card.card_name}.png`
+      : '/assets/playingcards/card_background.png'
+
+    if (card && card.id !== 0) {
+      const { card_name } = card
+      let randomDeluxe = ''
+
+      if (['alien', 'robber', 'seer', 'tanner', 'troublemaker', 'vampire', 'werewolf'].includes(card_name)) {
+        if (card_name === 'alien') {
+          randomDeluxe = Math.random() < 0.5 ? '_male' : '_female'
+        } else if (card_name === 'werewolf') {
+          randomDeluxe = `_${Math.floor(Math.random() * 3) + 1}`
+        } else {
+          randomDeluxe = `_${Math.floor(Math.random() * 2) + 1}`
+        }
+        imgSrc = `/assets/playingcards/${card_name}${randomDeluxe}.png`
+      }
+    }
 
     return (
       <StyledDealtCard>
