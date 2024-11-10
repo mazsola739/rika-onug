b61b877266b608cdd9e890095b8f6cdd280e4628
Rika <mazsola39@gmail.com>
2024-11-10 15:28:40 +0100

    useVerdict

--- onug-frontend/src/routes/Verdict/useVerdict.tsx
+++ onug-frontend/src/routes/Verdict/useVerdict.tsx
@@ -1,1 +1,37 @@
- 
+import { ARRIVE_VERDICT, REDIRECT, RESULT } from 'constant'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { propStore, riseAndRestStore, voteStore, wsStore } from 'store'
+
+export const useVerdict = () => {
+  const [firstTime, setFirstTime] = useState(true)
+  const navigate = useNavigate()
+  const { sendJsonMessage, lastJsonMessage } = wsStore.getWsCommunicationsBridge()
+
+  const room_id = sessionStorage.getItem('room_id')
+  const token = sessionStorage.getItem('token')
+
+  useEffect(() => {
+    if (sendJsonMessage && firstTime) {
+      setFirstTime(false)
+      sendJsonMessage({
+        type: ARRIVE_VERDICT,
+        room_id,
+        token
+      })
+    }
+  }, [sendJsonMessage, firstTime])
+
+  useEffect(() => {
+    if (lastJsonMessage?.type === RESULT && lastJsonMessage.success) {
+      propStore.setVoteResult(lastJsonMessage.vote_result)
+      propStore.setWinnerTeams(lastJsonMessage.winner_teams)
+      riseAndRestStore.setTableCenterCards(lastJsonMessage)
+      voteStore.revealResult(lastJsonMessage)
+    }
+
+    if (lastJsonMessage?.type === REDIRECT) {
+      navigate(lastJsonMessage.path)
+    }
+  }, [lastJsonMessage, navigate])
+}
