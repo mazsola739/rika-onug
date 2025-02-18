386a424046ac2724a36c3340c604afbf3ac13fc9
Rika <mazsola39@gmail.com>
2025-02-18 16:27:37 +0100

    change

--- onug-frontend/scripts/imageCompress.cjs
+++
@@ -1,8 +0,0 @@
-const sharp = require('sharp')
-
-sharp('public/assets/tokens/pic3634423-modified(1).png')
-  .webp({ lossless: true })
-  .toFile('public/assets/tokens/pic3634423-modified(1).webp', (err, info) => {
-    console.log(err)
-    console.log(info)
-  })
