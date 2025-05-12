708a82a9f2d4e51f4a058958a829683c7eebaa43
mazsola739 <mazsola39@gmail.com>
2025-05-12 13:29:26 +0200

    test alien

--- backend/src/scenes/roles/aliens/aliens.action.js
+++ backend/src/scenes/roles/aliens/aliens.action.js
@@ -21,37 +21,25 @@ export const aliensAction = (gamestate, token, title) => {
   let new_alien = []
   let new_alien_helper = []
 
-  
-  const evenOrOdd = alienKey[0].includes('even') ? 'Even' : 'Odd'
-  const isEvenOrOdd = alienKey[0].includes('even') || alienKey[0].includes('odd')
-  const isAny = alienKey[0].includes('any')
-  const isPlayer = alienKey[0].includes('identifier_player')
-
-  const currentPlayerNumber = getPlayerNumbersByGivenConditions(gamestate.players, 'currentPlayer', [], token)[0]
-  const aliensWithoutShield = getPlayerNumbersByGivenConditions(gamestate.players, 'alienWithoutShield', gamestate.positions.shielded_cards)
-  const aliensWithoutShieldIdentifiers = formatPlayerIdentifier(aliensWithoutShield)
-  
-  const anyWihtoutShield = getPlayerNumbersByGivenConditions(gamestate.players, 'allPlayersWithoutShield', gamestate.positions.shielded_cards)
-  const anyWihtoutShieldIdentifiers = formatPlayerIdentifier(anyWihtoutShield)
-  const evenOrOddPlayers = getPlayerNumbersByGivenConditions(gamestate.players, `nonAlien${evenOrOdd}`)
-  const evenOrOddPlayersIdentifiers = formatPlayerIdentifier(evenOrOddPlayers)
-  const selectablePlayerNumbers = isPlayer ? alienKey.filter(key => key.includes('identifier_player')).map(key => key.replace('identifier_player', 'player_')) : []
-  const alienKeyIdentifiers = formatPlayerIdentifier(selectablePlayerNumbers)
-  const message = isEvenOrOdd ? evenOrOddPlayersIdentifiers : isAny ? anyWihtoutShieldIdentifiers : alienKeyIdentifiers
-
-  const evenOrOddWithoutShield = getPlayerNumbersByGivenConditions(gamestate.players, `nonAlienWithoutShield${evenOrOdd}`, gamestate.positions.shielded_cards)
-  const evenOrOddWithoutShieldIdentifiers = formatPlayerIdentifier(evenOrOddWithoutShield)
-  const anyPlayers = getPlayerNumbersByGivenConditions(gamestate.players, 'allPlayers')
-  const anyPlayersIdentifiers = formatPlayerIdentifier(anyPlayers)
-  const newMessage = isEvenOrOdd ? evenOrOddWithoutShieldIdentifiers : anyPlayersIdentifiers
-
-
   switch (randomAlienInstruction) {
     case 'aliens_view': //aliens_view = 'Each alien may secretly view a card from: '
-    case 'aliens_allview': //aliens_allview = 'All aliens together may view a card from: '
+    case 'aliens_allview': {
+      //aliens_allview = 'All aliens together may view a card from: '
+      const isEvenOrOdd = alienKey[0].includes('even') || alienKey[0].includes('odd')
+      const evenOrOdd = alienKey[0].includes('even') ? 'Even' : 'Odd'
+      const isAny = alienKey[0].includes('any')
+      const isPlayer = alienKey[0].includes('identifier_player')
+      const evenOrOddPlayers = getPlayerNumbersByGivenConditions(gamestate.players, `nonAlien${evenOrOdd}`)
+      const evenOrOddPlayersIdentifiers = formatPlayerIdentifier(evenOrOddPlayers)
+      const anyWihtoutShield = getPlayerNumbersByGivenConditions(gamestate.players, 'allPlayersWithoutShield', gamestate.positions.shielded_cards)
+      const anyWihtoutShieldIdentifiers = formatPlayerIdentifier(anyWihtoutShield)
+      const selectablePlayerNumbers = isPlayer ? alienKey.filter(key => key.includes('identifier_player')).map(key => key.replace('identifier_player', 'player_')) : []
+      const alienKeyIdentifiers = formatPlayerIdentifier(selectablePlayerNumbers)
+      const message = isEvenOrOdd ? evenOrOddPlayersIdentifiers : isAny ? anyWihtoutShieldIdentifiers : alienKeyIdentifiers
       selectable_cards = isEvenOrOdd ? evenOrOddPlayers : isAny ? anyWihtoutShield : selectablePlayerNumbers
       privateMessage.push(...message)
       break
+    }
 
     case 'aliens_left': //aliens_left = 'Give your card to the alien on your left.'
     case 'aliens_right': //aliens_right = 'Give your card to the alien on your right.'
@@ -59,26 +47,39 @@ export const aliensAction = (gamestate, token, title) => {
         privateMessage.push('action_shielded')
       } else {
         const direction = randomAlienInstruction.includes('left') ? 'left' : 'right'
+        const currentPlayerNumber = getPlayerNumbersByGivenConditions(gamestate.players, 'currentPlayer', [], token)[0]
+        const aliensWithoutShield = getPlayerNumbersByGivenConditions(gamestate.players, 'alienWithoutShield', gamestate.positions.shielded_cards)
         const neighbor = getNeighborByPosition(aliensWithoutShield, currentPlayerNumber, direction)
         moveCards(gamestate, token, direction, aliensWithoutShield)
         privateMessage.push('action_moved_yours', ...formatPlayerIdentifier([neighbor]))
       }
       break
-    case 'aliens_show': //aliens_show = 'Show your alien card to the other aliens.'
+    case 'aliens_show': {
+      //aliens_show = 'Show your alien card to the other aliens.'
+      const aliensWithoutShield = getPlayerNumbersByGivenConditions(gamestate.players, 'alienWithoutShield', gamestate.positions.shielded_cards)
+      const aliensWithoutShieldIdentifiers = formatPlayerIdentifier(aliensWithoutShield)
       showCards = sawCards(gamestate, aliensWithoutShield, token)
       privateMessage.push('action_saw_card', ...aliensWithoutShieldIdentifiers)
       break
+    }
     case 'aliens_timer': //aliens_timer = 'You have shortened the game timer by one half.'
       gamestate.vote_timer /= 2
       privateMessage.push('action_timer')
       break
     case 'aliens_newalien': //aliens_newalien = 'Tap one of the fists to turn that player into an alien from: '
-    case 'aliens_alienhelper':
+    case 'aliens_alienhelper': {
       //aliens_alienhelper = "Tap one of the fists to turn that player into alien team, but isn't an alien from: "
-
+      const isEvenOrOdd = alienKey[0].includes('even') || alienKey[0].includes('odd')
+      const evenOrOdd = alienKey[0].includes('even') ? 'Even' : 'Odd'
+      const evenOrOddWithoutShield = getPlayerNumbersByGivenConditions(gamestate.players, `nonAlienWithoutShield${evenOrOdd}`, gamestate.positions.shielded_cards)
+      const evenOrOddWithoutShieldIdentifiers = formatPlayerIdentifier(evenOrOddWithoutShield)
+      const anyPlayers = getPlayerNumbersByGivenConditions(gamestate.players, 'allPlayers')
+      const anyPlayersIdentifiers = formatPlayerIdentifier(anyPlayers)
+      const newMessage = isEvenOrOdd ? evenOrOddWithoutShieldIdentifiers : anyPlayersIdentifiers
       selectable_cards = isEvenOrOdd ? evenOrOddWithoutShield : anyPlayers
       privateMessage.push(...newMessage)
       break
+    }
   }
 
   const obligatory = ['aliens_left', 'aliens_right', 'aliens_show', 'aliens_newalien', 'aliens_alienhelper'].includes(randomAlienInstruction)
